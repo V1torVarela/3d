3831a4ea34ea0d0c3c27f299110f6e2602f65e2a
Vitor Varela <kryzshop@gmail.com>
2022-08-15 00:58:46 +0100

    Third Commit

--- src/script.js
+++ src/script.js
@@ -40,6 +40,48 @@ pointLight.position.y = 3;
 pointLight.position.z = 4;
 scene.add(pointLight);
 
+// Light 2
+const pointLight2 = new THREE.PointLight(0xff0000, 2);
+pointLight2.position.set(-1.86, 1.54, -1.13);
+pointLight2.intensity = 3.12;
+
+scene.add(pointLight2);
+
+const light1 = gui.addFolder("Light 1"); //Separação de luzes no Control
+
+light1.add(pointLight2.position, "y").min(-3).max(3).step(0.01);
+light1.add(pointLight2.position, "x").min(-6).max(6).step(0.01);
+light1.add(pointLight2.position, "z").min(-3).max(3).step(0.01);
+light1.add(pointLight2, "intensity").min(0).max(10).step(0.01);
+
+//const pointLightHelper = new THREE.PointLightHelper(pointLight2, 1);
+//scene.add(pointLightHelper);
+
+// Light 3
+const pointLight3 = new THREE.PointLight(0xe1ff, 2);
+pointLight3.position.set(3.98, -1.91, -1.98);
+pointLight3.intensity = 5.01;
+
+scene.add(pointLight3);
+
+const light2 = gui.addFolder("Light 2"); //Separação de luzes no Control
+
+light2.add(pointLight3.position, "y").min(-3).max(3).step(0.01);
+light2.add(pointLight3.position, "x").min(-6).max(6).step(0.01);
+light2.add(pointLight3.position, "z").min(-3).max(3).step(0.01);
+light2.add(pointLight3, "intensity").min(0).max(10).step(0.01);
+
+const light2Color = {
+  color: 0xff0000,
+};
+
+light2.addColor(light2Color, "color").onChange(() => {
+  pointLight3.color.set(light2Color.color);
+});
+
+//const pointLightHelper2 = new THREE.PointLightHelper(pointLight3, 1);
+//scene.add(pointLightHelper2);
+
 /**
  * Sizes
  */
@@ -95,13 +137,36 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  * Animate
  */
 
+document.addEventListener("mousemove", onDocumentMouseMove);
+
+let mouseX = 0;
+let mouseY = 0;
+
+let targetx = 0;
+let targetY = 0;
+
+const windowX = window.innerWidth / 2;
+const windowY = window.innerWidth / 2;
+
+function onDocumentMouseMove(event) {
+  mouseX = event.clientX - windowX;
+  mouseY = event.clientY - windowY;
+}
+
 const clock = new THREE.Clock();
 
 const tick = () => {
+  targetx = mouseX * 0.001;
+  targetY = mouseY * 0.001;
+
   const elapsedTime = clock.getElapsedTime();
 
   // Update objects
-  sphere.rotation.y = 0.5 * elapsedTime;
+  sphere.rotation.y = 0.05 * elapsedTime;
+
+  sphere.rotation.y += 0.2 * (targetx - sphere.rotation.y);
+  sphere.rotation.y += -0.02 * (targetY - sphere.rotation.x);
+  sphere.rotation.z += -0.02 * (targetY - sphere.rotation.x);
 
   // Update Orbital Controls
   // controls.update()
